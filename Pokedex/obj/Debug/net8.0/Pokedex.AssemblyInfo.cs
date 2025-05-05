aca4963ebc68b58ffd3a349e88e6fdde3f39870e
Gus - Windows <gustavo.caetano13@gmail.com>
2025-05-05 15:43:34 -0300

    atualiza html e css para pokedex completa

--- Pokedex/obj/Debug/net8.0/Pokedex.AssemblyInfo.cs
+++ Pokedex/obj/Debug/net8.0/Pokedex.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c467f78d8b3aa0099d08182b94bf2582341dfe3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bee1cccf6d6ad62a1556e4e8f818aeec5504c6cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
